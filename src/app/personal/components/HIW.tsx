3fcdd7061b695cbae87be18854e09e01f32671fa
William Rainey <williamhrainey@gmail.com>
2023-05-18 11:23:32 -0700

    added basic fade to how it works component

--- src/app/personal/components/HIW.tsx
+++ src/app/personal/components/HIW.tsx
@@ -58,34 +58,22 @@ export const HIW = () => {
 
     
   return (
-    <div className='realitive mb-20 h-[800px]'>
-         <img src='./side_drip_short.png' className='absolute w-[40%] left-[120px] top-0  onscroll'/>
+    <div className='realitive mb-20'>
 
-        <div className={/* scrollPosition >= 800 && scrollPosition <= 900 ?  *//* isVisible ? */ 'flex flex-col items-center h-[640px] overflow-auto cardCon border-2 border-black' /* : 'hidden'  */}  onScroll={checkPosition} ref={scrollRef}>
-
-                <ul className='w-10/12 flex flex-col items-center pt-[650px]'>
-                    {/* <Fade delay={300} className='sticky top-[100px]'> */}
-                        <p className='text-6xl font-semibold text-center mb-[100px] '>How it works:</p>
-                    {/* </Fade> */}
-                    {/* <Fade delay={300} /> */}
-                        <li className='w-6/12 mb-[100px] sticky top-[200px]'><span className='font-bold text-xl mr-2'>1. </span>Design your character(s): Provide details about your desired artwork, including size, medium, subject matter, and any reference images or inspiration. The more information you provide, the better we can tailor the artwork to your vision.</li>
-                    {/* </Fade> */}
+        <Fade delay={700}>
+            <img src='./side_drip_short.png' className='absolute w-[40%] left-[120px] onscroll'/>
+            <div className={/* scrollPosition >= 800 && scrollPosition <= 900 ?  *//* isVisible ? */ 'flex flex-col items-center mt-[145px]' /*overflow-auto cardCon */   /* : 'hidden'  */}  /* onScroll={checkPosition} */ ref={scrollRef}>
                     
-                    {/* <Fade delay={300} /> */}
-                        <li className='mb-[100px] w-6/12 sticky top-[300px]'><span className='font-bold text-xl mr-2'>2. </span>Based on your project specifications, we&apos;ll provide a custom quote for your art commission. Our transparent pricing ensures no surprises along the way.</li>
-                    {/* </Fade> */}
+                        <ul className='w-10/12 flex flex-col items-center'>
+                                <p className='text-6xl font-semibold text-center mb-[50px] ' /* mb-[100px] */>How it works:</p>
+                                <li className='w-6/12 mb-[25px]' /* mb-[100px] sticky top-[200px] */><span className='font-bold text-xl mr-2'>1. </span>Design your character(s): Provide details about your desired artwork, including size, medium, subject matter, and any reference images or inspiration. The more information you provide, the better we can tailor the artwork to your vision.</li>
+                                <li className='w-6/12 mb-[25px]' /* mb-[100px] sticky top-[300px]*/><span className='font-bold text-xl mr-2'>2. </span>Based on your project specifications, we&apos;ll provide a custom quote for your art commission. Our transparent pricing ensures no surprises along the way.</li>
+                                <li className='w-6/12 mb-[25px]' /* mb-[100px] sticky top-[375px]*/><span className='font-bold text-xl mr-2'>3. </span>Once you&apos;re happy with the quote, we will reserve a payment and connect you with the artist to begin the creative process. Collaborate directly with the artist to refine your vision and watch your artwork come to life.</li>
+                                <li className='w-6/12 mb-[25px]' /* mb-[85px] sticky top-[375px]*/><span className='font-bold text-xl mr-2'>4. </span>Upon completion, you&apos;ll recieve your stunning custom art piece, ready to display and enjoy for years to come. Once you&apos;re happy, you can choose when to release the reserved payment directly to the artist!</li>
+                        </ul>
                     
-                    {/* <Fade delay={300} > */}
-                        <li className='mb-[100px] w-6/12 sticky top-[375px]'><span className='font-bold text-xl mr-2'>3. </span>Once you&apos;re happy with the quote, we will reserve a payment and connect you with the artist to begin the creative process. Collaborate directly with the artist to refine your vision and watch your artwork come to life.</li>
-                    {/* </Fade> */}
-                    
-                    {/* <Fade delay={300} > */}
-                        <li className='mb-[85px] w-6/12 sticky top-[375px]'><span className='font-bold text-xl mr-2'>4. </span>Upon completion, you&apos;ll recieve your stunning custom art piece, ready to display and enjoy for years to come. Once you&apos;re happy, you can choose when to release the reserved payment directly to the artist!</li>
-                    {/* </Fade> */}
-                </ul>
-                 
-        </div>
-        <div  className='w-full h-[100px] border-2 border-black'></div>  
+            </div>
+        </Fade> 
     </div>
   )
 }
