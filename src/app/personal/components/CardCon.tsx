479f1349c8662d07a25809040c8c44ccc933edfc
linds0202 <lindsayarainey@gmail.com>
2023-06-07 11:08:09 -0700

    add spring to HIW on personal landing page

--- src/app/personal/components/CardCon.tsx
+++ src/app/personal/components/CardCon.tsx
@@ -28,11 +28,18 @@ export default function CardCon({info, scroll}:Data) {
         <div className={scroll ? "relative p-4 bg-[#262626] rounded-3xl" : "relative p-4"}>
           <div className={scroll ? "overflow-x-auto flex-nowrap pb-4 flex justify-between horz-scroll-con relative rounded-2xl" : "p-4 flex justify-around relative"}>
             {info.map(el =>
-                <div key={el.style} className={scroll ? 'w-[30%] h-[500px] mx-4 flex flex-col items-center p-4 hover:cursor-pointer' : 'w-3/12 flex flex-col items-center drip-card hover:cursor-pointer'} style={scroll ? {flex: '0 0 auto'} : {}} onClick={() => router.push(`/portraits/?styleOne=${el.abrv}`)}>
-                 
-                    <img src={el.img} alt="it's pepe" className={scroll ? "w-full h-full rounded-lg" : "w-full h-[90%]"}/>
-                    <img src="./frame.png" alt="frame" className={scroll ? "hidden" : "absolute w-[30%] mr-[2.5%] h-[730px] top-[3px] pr-px drip transition duration-200 ease-in-out"}/>
-                  <button className={scroll ? 'text-2xl font-semibold text-black bg-white absolute z-40 bottom-7 w-[20.39%] py-px border-2 border-black hover:border-2 hover:border-black transition-all ease-in-out duration-200' : "text-2xl font-semibold text-black bg-white absolute z-40 bottom-14 w-[20%] py-px border-2 border-black hover:border-2 hover:border-black transition-all ease-in-out duration-200 "}>{el.style}</button>
+                <div 
+                  key={el.style} 
+                  className={scroll ? 'w-[30%] h-[500px] mx-4 flex flex-col items-center p-4 hover:cursor-pointer' : 'w-3/12 flex flex-col items-center drip-card hover:cursor-pointer'} 
+                  style={scroll ? {flex: '0 0 auto'} : {}} onClick={() => router.push(`/portraits/?styleOne=${el.abrv}`)}
+                >
+                  <img src={el.img} alt="it's pepe" className={scroll ? "w-full h-full rounded-lg" : "w-full h-[90%]"}/>
+
+                  <img src="./frame.png" alt="frame" className={scroll ? "hidden" : "absolute w-[30%] mr-[2.5%] h-[730px] top-[3px] pr-px drip transition duration-200 ease-in-out"}/>
+
+                  <button className={scroll ? 'text-2xl font-semibold text-black bg-white absolute z-40 bottom-7 w-[20.39%] py-px border-2 border-black hover:border-2 hover:border-black transition-all ease-in-out duration-200' : "text-2xl font-semibold text-black bg-white absolute z-40 bottom-14 w-[20%] py-px border-2 border-black hover:border-2 hover:border-black transition-all ease-in-out duration-200 "}>
+                    {el.style}
+                  </button>
                 </div>
               )}
           </div>
--- src/app/personal/components/HIW.tsx
+++ src/app/personal/components/HIW.tsx
@@ -13,7 +13,8 @@ const HIW = () => {
         show: {
             opacity: 1,
             transition: {
-                staggerChildren: 1,
+                when: "beforeChildren",
+                staggerChildren: 0.75,
             }
         }
 
@@ -31,7 +32,8 @@ const HIW = () => {
             opacity: 1,
             x: 0,
             transition: {
-                duration: .5
+                type: "spring",
+                duration: .3
             }
         }
     }
@@ -40,12 +42,32 @@ const HIW = () => {
     <div className='flex flex-col items-center pt-[100px] mb-[200px] text-black'>
         <div className='w-full mb-[75px] pl-[13%]'>
             <div className='w-[50%] flex flex-col items-start'>
-                <motion.h2 className='text-6xl font-bold mb-2' initial={{opacity: 0, x: -200}} whileInView={{opacity: 1, x: 0}} transition={{ease: 'easeInOut', duration: .5}}>How it works:</motion.h2>
-                <motion.div className='h-[2px] bg-black pl-2' initial={{opacity: 0, left: 0, width: 0, x:200}} whileInView={{opacity: 1, width: '45%', x:35}} transition={{ease: 'easeInOut', duration: .5}}/>
+                <motion.h2 
+                    className='text-6xl font-bold mb-2' 
+                    initial={{opacity: 0, x: -200}} 
+                    whileInView={{opacity: 1, x: 0}} 
+                    transition={{ease: 'easeInOut', duration: .5}}
+                    viewport={{amount: 0.4, once: true}}
+                >
+                    How it works:
+                </motion.h2>
+                <motion.div 
+                className='h-[2px] bg-black pl-2' 
+                initial={{opacity: 0, left: 0, width: 0, x:200}} 
+                whileInView={{opacity: 1, width: '45%', x:35}} 
+                transition={{ease: 'easeInOut', duration: .5}}
+                viewport={{amount: 0.4, once: true}}
+            />
             </div>
         </div>
         
-        <motion.ul className='w-10/12 flex flex-col pl-[5%] border-l-2 border-black space-y-[40px]' variants={list} initial='hidden' whileInView='show' viewport={{amount: 0.4}}>       
+        <motion.ul 
+            className='w-10/12 flex flex-col pl-[5%] border-l-2 border-black space-y-[20px]' 
+            variants={list} 
+            initial='hidden' 
+            whileInView='show' 
+            viewport={{amount: 0.4, once: true}}
+        >       
             <motion.li className='w-10/12 text-lg'  variants={listItemVarient} /* transition={
                 {delay:.5,
                 duration: .5}} */><span className='font-bold text-xl mr-2'>1. </span>Design your character(s): Provide details about your desired artwork, including size, medium, subject matter, and any reference images or inspiration. The more information you provide, the better we can tailor the artwork to your vision.</motion.li>
