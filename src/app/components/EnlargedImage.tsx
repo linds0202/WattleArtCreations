5e14a5dddb4cae4e979bd0b6c9593615344a9e49
linds0202 <lindsayarainey@gmail.com>
2023-09-18 17:06:19 -0700

    test x container

--- src/app/components/EnlargedImage.tsx
+++ src/app/components/EnlargedImage.tsx
@@ -31,18 +31,19 @@ const EnlargedImage = ({openImage, setOpenImage, src, date, final}: EnlargedImag
             maxWidth='lg'
             PaperProps={{ sx: { maxHeight: '100vh', p: 6, backgroundColor: "white"} }}
         >
-            <IconButton onClick={() => setOpenImage(false)} className='absolute top-2 right-2 text-white'>
-                <CloseIcon className='text-black hover:text-red-600'/>
-            </IconButton>
-            
-            <DialogContent
-                style={{height:'80vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', position: 'relative' }}
-            >
-                <img onContextMenu={(e)=> handleRightClickImg(e)} src={src} className='max-h-[80%] w-auto' alt='enlarged final image'/>
-                {final && <div onContextMenu={(e)=> handleRightClick(e)} className='absolute top-0 left-0 bg-sky-500/10 h-[100%] w-[100%] border-2 border-[#282828]'></div>}
-                {final && <p className='mt-4'>Submitted: {new Date(date.toDate()).toLocaleDateString("en-US")}</p>}
-            </DialogContent>
-                    
+            <div className='w-full relative'>
+                <IconButton onClick={() => setOpenImage(false)} className='absolute top-2 right-2 w-1/12 text-white'>
+                    <CloseIcon className='text-black hover:text-red-600'/>
+                </IconButton>
+                
+                <DialogContent
+                    style={{height:'80vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', position: 'relative' }}
+                >
+                    <img onContextMenu={(e)=> handleRightClickImg(e)} src={src} className='max-h-[80%] w-auto' alt='enlarged final image'/>
+                    {final && <div onContextMenu={(e)=> handleRightClick(e)} className='absolute top-0 left-0 bg-sky-500/10 h-[100%] w-[100%] border-2 border-[#282828]'></div>}
+                    {final && <p className='mt-4'>Submitted: {new Date(date.toDate()).toLocaleDateString("en-US")}</p>}
+                </DialogContent>
+            </div>   
         </Dialog>
     )
 }
