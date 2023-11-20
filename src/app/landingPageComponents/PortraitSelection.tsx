4b86e5ee4d0c72844bfca24ffce706a49403ed54
linds0202 <lindsayarainey@gmail.com>
2023-11-20 14:42:12 -0800

    update image

--- src/app/landingPageComponents/PortraitSelection.tsx
+++ src/app/landingPageComponents/PortraitSelection.tsx
@@ -303,7 +303,7 @@ const PortraitSelection = ({ mode, setMode }: ModeProps) => {
             </div>
 
             <div className="relative w-full h-[100vh] flex justify-around items-center mt-[50px] mb-[50px] pt-[100px] pb-[50px] text-white bg-gradient-to-b from-black from-10% to-[#282828] to-95% z-10" >
-                <object type="image/svg+xml" data="images/HIWIcons/brick_final.svg" className="absolute top-0 left-0 w-[100%] h-[100%]"/>
+                <object type="image/svg+xml" data="images/HIWIcons/brick_final2.svg" className="absolute top-0 left-0 w-[100%] h-[100%]"/>
                 <div className="absolute top-0 left-0 w-[90%] h-[100%] flex justify-around items-center z-50">
                     <motion.div className="w-[500px] h-[550px] ml-20 object-cover rounded-xl" >
                         <Carousel 
