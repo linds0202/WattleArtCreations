52d172f94deb3c34e7adfd34e2d6eb20fae20bbe
linds0202 <lindsayarainey@gmail.com>
2023-12-01 13:37:27 -0800

    style selection testimonials

--- src/app/landingPageComponents/Home2_testimonial.tsx
+++ src/app/landingPageComponents/Home2_testimonial.tsx
@@ -11,7 +11,7 @@ const Home2_testimonial = ({ test }: TestimonialProps) => {
                 className="w-full h-full mx-2 text-white text-xl p-4 rounded-xl border border-[#ffffff]/25 flex flex-col justify-between items-center hover:shadow-[0_0_40px_-5px_rgba(255,255,255,0.5)] hover:border-[#ffffff]/50"
             >
                 <img src={test.imgUrl} className="w-[250px] h-[250px] object-cover object-top rounded-xl mb-8 top-0 left-0"/>
-                <p className="h-full font-light text-xl">&ldquo;{test.text.split(' ').slice(0, 40).join(' ')}{test.text.split(' ').length > 40 ? '...' : ''}&ldquo;</p>
+                <p className="h-full font-light text-xl">&ldquo;{test.text.split(' ').slice(0, 30).join(' ')}{test.text.split(' ').length > 40 ? '...' : ''}&ldquo;</p>
                 <p className='self-end font-semibold'>-{test.customerDisplayName}</p>
             </div>
         </div>
